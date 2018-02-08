e8d5d7460311ecd0a66212950b46826a71713868
Dave Kelly <dave.a.kelly@gmail.com>
2018-02-08 22:04:27 +0000

    Buttons implemented

--- basic1.py
+++ basic1.py
@@ -3,65 +3,74 @@ import basic_form
 
 
 class TestFrame (basic_form.basic_frame):
-   def __init__(self, Parent, netlist):
-      # Initialize parent class
-      basic_form.basic_frame.__init__(self, Parent)
+    def __init__(self, Parent, netlist):
+        # Initialize parent class
+        basic_form.basic_frame.__init__(self, Parent)
 
-      # Now populate the netlist
+        # Now populate the netlist
 
-      self.m_grid1.SetCellValue(0, 0, 'Test')
-      print(netlist)
-      for row in range (0, len(netlist)):
-         if row < 30:
-            #print (netlist [row])
-            self.m_grid1.SetCellValue(row, 0, netlist[row][1])
-            self.m_grid1.SetCellValue(row, 1, "1")
+        self.m_grid1.SetCellValue(0, 0, 'Test')
+        print(netlist)
+        for row in range (0, len(netlist)):
+           if row < 30:
+               #print (netlist [row])
+               self.m_grid1.SetCellValue(row, 0, netlist[row][1])
+               self.m_grid1.SetCellValue(row, 1, "1")
+
+    def Cancel( self, event ):
+        event.Skip(True)
+        self.Close()
+      
+	
+    def Ok( self, event ):
+        event.Skip(True)
+	
 
 
 def load_netlist(filename):
 
-   netlist = []
+    netlist = []
 
-   with open(filename, 'r') as f:
-      lines = f.readlines()
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-      for line in lines:
-         row_data = []
-         if line.find('(net ') > -1:
-            # key words are code and name
-            fields = line.split (' ')
+        for line in lines:
+            row_data = []
+            if line.find('(net ') > -1:
+                # key words are code and name
+                fields = line.split (' ')
 
-            have_field = False
-            for index in range (0, len(fields)):
-               if fields [index] == '(code':
-                  row_data.append(fields [index + 1][:-1])
+                have_field = False
+                for index in range (0, len(fields)):
+                    if fields [index] == '(code':
+                        row_data.append(fields [index + 1][:-1])
 
-               if fields [index] == '(name':
-                  row_data.append(fields[index + 1].strip()[:-1])
-                  have_field = True
+                    if fields [index] == '(name':
+                        row_data.append(fields[index + 1].strip()[:-1])
+                        have_field = True
 
-            if have_field:
-               netlist.append(row_data)
+                if have_field:
+                    netlist.append(row_data)
 
             #Now find the end of the net
 
       #print(netlist)
 
-   return netlist
+    return netlist
 
 def main ():
-   print ('Test basic wxWidget form')
+    print ('Test basic wxWidget form')
 
-   netlist = load_netlist('STM32_TestBoard1.net')
-   app = wx.App(False)
+    netlist = load_netlist('STM32_TestBoard1.net')
+    app = wx.App(False)
 
-   frame = TestFrame(None, netlist)
+    frame = TestFrame(None, netlist)
 
-   #show the frame
-   frame.Show(True)
-   #start the applications
-   app.MainLoop()
+    #show the frame
+    frame.Show(True)
+    #start the applications
+    app.MainLoop()
 
 
 if __name__ == '__main__':
-   main ()
+    main ()
--- basic_form.py
+++ basic_form.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 ###########################################################################
-## Python code generated with wxFormBuilder (version Feb  5 2018)
+## Python code generated with wxFormBuilder (version Feb  8 2018)
 ## http://www.wxformbuilder.org/
 ##
 ## PLEASE DO *NOT* EDIT THIS FILE!
@@ -16,31 +16,31 @@ import wx.grid
 ###########################################################################
 
 class basic_frame ( wx.Frame ):
-
+	
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"wxWidget Basic Test", pos = wx.DefaultPosition, size = wx.Size( 676,570 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-
-		# self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-
+		
+		#self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		
 		bSizer2 = wx.BoxSizer( wx.VERTICAL )
-
+		
 		self.m_notebook1 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
-
+		
 		bSizer2.Add( self.m_notebook1, 1, wx.EXPAND |wx.ALL, 5 )
-
+		
 		bSizer21 = wx.BoxSizer( wx.HORIZONTAL )
-
+		
 		bSizerLeft = wx.BoxSizer( wx.VERTICAL )
-
+		
 		self.m_grid1 = wx.grid.Grid( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 400,-1 ), 0 )
-
+		
 		# Grid
 		self.m_grid1.CreateGrid( 30, 2 )
 		self.m_grid1.EnableEditing( True )
 		self.m_grid1.EnableGridLines( True )
 		self.m_grid1.EnableDragGridSize( False )
 		self.m_grid1.SetMargins( 0, 0 )
-
+		
 		# Columns
 		self.m_grid1.SetColSize( 0, 227 )
 		self.m_grid1.SetColSize( 1, 87 )
@@ -50,47 +50,59 @@ class basic_frame ( wx.Frame ):
 		self.m_grid1.SetColLabelValue( 0, u"Net Name" )
 		self.m_grid1.SetColLabelValue( 1, u"Visible" )
 		self.m_grid1.SetColLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
-
-		self.m_grid1.SetColFormatBool (1)
+		
+		self.m_grid1.SetColFormatBool(1)
 
 		# Rows
 		self.m_grid1.EnableDragRowSize( True )
 		self.m_grid1.SetRowLabelSize( 80 )
 		self.m_grid1.SetRowLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
-
+		
 		# Label Appearance
-
+		
 		# Cell Defaults
 		self.m_grid1.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
 		bSizerLeft.Add( self.m_grid1, 0, wx.ALL, 5 )
-
-
+		
+		
 		bSizer21.Add( bSizerLeft, 3, wx.EXPAND, 5 )
-
+		
 		bSizerRight = wx.BoxSizer( wx.VERTICAL )
-
+		
 		m_sdbSizer1 = wx.StdDialogButtonSizer()
 		self.m_sdbSizer1OK = wx.Button( self, wx.ID_OK )
 		m_sdbSizer1.AddButton( self.m_sdbSizer1OK )
 		self.m_sdbSizer1Cancel = wx.Button( self, wx.ID_CANCEL )
 		m_sdbSizer1.AddButton( self.m_sdbSizer1Cancel )
 		m_sdbSizer1.Realize();
-
+		
 		bSizerRight.Add( m_sdbSizer1, 1, wx.EXPAND, 5 )
-
-
+		
+		
 		bSizer21.Add( bSizerRight, 1, wx.EXPAND, 5 )
-
-
+		
+		
 		bSizer2.Add( bSizer21, 1, wx.EXPAND, 5 )
-
-
+		
+		
 		self.SetSizer( bSizer2 )
 		self.Layout()
-
+		
 		self.Centre( wx.BOTH )
-
+		
+		# Connect Events
+		self.m_sdbSizer1Cancel.Bind( wx.EVT_BUTTON, self.Cancel )
+		self.m_sdbSizer1OK.Bind( wx.EVT_BUTTON, self.Ok )
+	
 	def __del__( self ):
 		pass
-
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def Cancel( self, event ):
+		event.Skip()
+	
+	def Ok( self, event ):
+		event.Skip()
+	
 
